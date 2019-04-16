a8e88cdb169424bfb3ac73ce703081cd2689a1f3
Felipe Valencia <fvalencia@vc-soft.com>
2019-04-16 17:49:50 -0500

    Began Calculator Work

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -3,32 +3,44 @@ import {
   OnInit,
   Injectable,
   Inject,
-  PLATFORM_ID
+  PLATFORM_ID,
+  Input
 } from '@angular/core';
-import { isPlatformBrowser, isPlatformServer } from '@angular/common';
-import { DOCUMENT } from '@angular/common';
 import * as Chart from 'chart.js';
+import { Entry } from '../entry';
+import { WeightService } from '../weight.service';
 @Component({
   selector: 'app-graph',
   templateUrl: './graph.component.html',
   styleUrls: ['./graph.component.css']
 })
 export class GraphComponent implements OnInit {
+  @Input() weightEntries: Entry[];
+  dates: Date[];
+  weights: number[];
   type = 'line';
-  data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-      {
-        borderColor: 'rgb(220,20,60)',
-        label: 'My First dataset',
-        data: [65, 59, 80, 81, 56, 55, 40]
-      }
-    ]
-  };
-  options = {
-    responsive: true,
-    maintainAspectRatio: false
-  };
+  data: Object;
+  options: Object;
+  constructor(weightService: WeightService) {
+    this.weightEntries.forEach(entry => {
+      this.dates.push(entry.date);
+      this.weights.push(entry.weight);
+    });
+    this.data = {
+      labels: this.dates,
+      datasets: [
+        {
+          borderColor: 'rgb(220,20,60)',
+          label: 'Weight Entries',
+          data: this.weights
+        }
+      ]
+    };
+    this.options = {
+      responsive: true,
+      maintainAspectRatio: false
+    };
+  }
 
   ngOnInit() {}
 }
